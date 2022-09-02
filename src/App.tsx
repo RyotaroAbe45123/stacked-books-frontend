8f494618f13318d64e97266302b5cd2f57fd4bef
RyotaroAbe <ryotaroynwa1225@gmial.com>
2022-09-02 07:56:28 +0000

    fix: confirm env

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  console.log(process.env.REACT_APP_API_BASE_URL);
   return (
     <div className="App">
       <header className="App-header">
