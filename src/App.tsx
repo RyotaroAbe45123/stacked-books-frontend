578fd39b10f89e8e582abc3cb52f14a88966964d
RyotaroAbe <ryotaroynwa1225@gmial.com>
2022-09-02 07:35:38 +0000

    fix: husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import logo from "./logo.svg"
-import "./App.css"
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>ppp</p>
+        <p>pppp</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
