b0551bbce7c561256035042376bd9e3ce90de006
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-09-24 01:27:02 +0000

    update: arrange table list data

--- src/components/BookTableList.tsx
+++ src/components/BookTableList.tsx
@@ -7,8 +7,10 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
+import { useCallback, useEffect, useState } from "react";
 import { MdBarChart } from "react-icons/md";
 import { Stack } from "types/api";
+import { TableListDataType } from "types/data";
 import { Statistics } from "./Statistics";
 
 type Props = {
@@ -17,21 +19,49 @@ type Props = {
 };
 
 export const BookTableList = ({ data, isLoading }: Props) => {
+  const [tableListData, setTableListData] = useState<TableListDataType[]>();
+
+  const convertTimeStampToDate = (timeStamp: string) => {
+    const date = new Date(timeStamp);
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+  };
+
+  // 最新の5件を選択
+  const selectData = useCallback((stacks: Stack[]): TableListDataType[] => {
+    const tableListData: TableListDataType[] = stacks.map((stack) => {
+      return {
+        date: convertTimeStampToDate(stack.timestamp),
+        price: stack.price,
+        pages: stack.pages,
+      };
+    });
+    tableListData.sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+    );
+    return tableListData.slice(0, 5);
+  }, []);
+
+  useEffect(() => {
+    if (data !== undefined) {
+      setTableListData(selectData(data));
+    }
+  }, [data, selectData]);
+
   return (
-    <Statistics title="table" icon={MdBarChart} isLoading={isLoading}>
+    <Statistics title="Table" icon={MdBarChart} isLoading={isLoading}>
       <TableContainer>
-        <Table variant="simple">
+        <Table size="md">
           <Thead>
             <Tr>
-              <Th>time</Th>
-              <Th>price</Th>
-              <Th>pages</Th>
+              <Th>積んだ日付</Th>
+              <Th>総金額</Th>
+              <Th>総ページ数</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {data?.map((d, i) => (
+            {tableListData?.map((d, i) => (
               <Tr key={i}>
-                <Td>{d.timestamp}</Td>
+                <Td>{d.date}</Td>
                 <Td>{d.price.toLocaleString()}</Td>
                 <Td>{d.pages.toLocaleString()}</Td>
               </Tr>
--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -2,3 +2,8 @@ export type BarChartDataType = {
   month: number;
   num: number;
 };
+export type TableListDataType = {
+  date: string;
+  price: number;
+  pages: number;
+};
