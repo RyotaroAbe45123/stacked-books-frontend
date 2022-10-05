aaeece65388ab604c1eb94995839c5422e3e617e
RyotaroAbe45123 <ryotaroynwa1225@gmail.com>
2022-10-05 00:24:53 +0000

    update: tag to small statistics

--- src/components/dashboard/SmallStatistics.tsx
+++ src/components/dashboard/SmallStatistics.tsx
@@ -4,24 +4,31 @@ import {
   Icon,
   Skeleton,
   Stat,
+  StatHelpText,
   StatLabel,
   StatNumber,
+  Tag,
 } from "@chakra-ui/react";
 import { theme } from "theme/theme";
 import { Card } from "../Card";
 import { IconType } from "react-icons";
+import { words } from "utils/words";
 
 type SmallStatisticsProps = {
   icon: IconType;
   title: string;
+  tag: string;
   value: string;
+  unit: string;
   isLoading: boolean;
 };
 
 export const SmallStatistics = ({
   icon,
   title,
+  tag,
   value,
+  unit,
   isLoading,
 }: SmallStatisticsProps) => {
   return (
@@ -37,16 +44,31 @@ export const SmallStatistics = ({
         >
           <Icon as={icon} color={theme.activeColor} w="24px" h="24px" />
         </Flex>
-        <Box marginLeft="20px">
+        <Box marginLeft="20px" marginTop="5%">
           <Stat>
-            <StatLabel lineHeight="100%" fontSize="sm">
-              {title}
-            </StatLabel>
+            <Flex alignItems="center" marginBottom="0px">
+              <StatLabel lineHeight="100%" fontSize="sm">
+                {title}
+              </StatLabel>
+              <Tag
+                marginLeft="10px"
+                size="sm"
+                borderWidth="2px"
+                borderColor={
+                  tag === words.dashboard.books.statTag.total
+                    ? theme.activeColor
+                    : theme.inactiveColor
+                }
+              >
+                {tag}
+              </Tag>
+            </Flex>
             {isLoading ? (
               <Skeleton color={theme.subColor} height={45} width={100} />
             ) : (
               <StatNumber fontSize="3xl">{value}</StatNumber>
             )}
+            <StatHelpText>{unit}</StatHelpText>
           </Stat>
         </Box>
       </Flex>
