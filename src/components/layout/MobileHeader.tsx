fc323022b7ec1784d01394c70c951a72af10b7e7
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-13 00:29:48 +0900

    update: links

--- src/components/layout/MobileHeader.tsx
+++ src/components/layout/MobileHeader.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  Divider,
   Drawer,
   DrawerBody,
   DrawerCloseButton,
@@ -8,13 +9,14 @@ import {
   DrawerHeader,
   DrawerOverlay,
   Flex,
+  HStack,
   Icon,
   IconButton,
   Text,
   useDisclosure,
 } from "@chakra-ui/react";
 import { LogoutButton } from "auth/LogoutButton";
-import { useLocation, useNavigate } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { routes } from "Routes";
 import { theme } from "theme/theme";
 import { words } from "utils/words";
@@ -25,6 +27,10 @@ export const MobileHeader = () => {
   const navigate = useNavigate();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const activeRoute = (routeName: string) => {
+    return location.pathname.includes(routeName);
+  };
 
   const getActivePageName = (pathName: string) => {
     const pageName = routes.find((route) => route.path === pathName);
@@ -54,22 +60,84 @@ export const MobileHeader = () => {
           />
           <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
             <DrawerOverlay />
-            <DrawerContent>
+            <DrawerContent maxW="60%">
               <DrawerCloseButton />
-              <DrawerHeader>Stacked Books</DrawerHeader>
+              <DrawerHeader>
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  h="50px"
+                >
+                  <NavLink to="/home" onClick={onClose}>
+                    <Text fontWeight="bold">{words.sidebar.title}</Text>
+                  </NavLink>
+                </Box>
+              </DrawerHeader>
+              <Divider />
               <DrawerBody>
-                <p>Home</p>
-                <p>Register</p>
-                <p>Books</p>
+                <Box ps="20px" pe="1px">
+                  {routes.map((route) => (
+                    <NavLink key={route.name} to={route.path} onClick={onClose}>
+                      <Box>
+                        <HStack py="5px" ps="10px">
+                          <Flex
+                            w="100%"
+                            alignItems="start"
+                            justifyContent="center"
+                          >
+                            <Box
+                              me="18px"
+                              mt="2px"
+                              color={
+                                activeRoute(route.path.toLowerCase())
+                                  ? theme.activeColor
+                                  : theme.inactiveColor
+                              }
+                            >
+                              <Icon
+                                as={route.icon}
+                                width="20px"
+                                height="20px"
+                                color="inherit"
+                              />
+                            </Box>
+                            <Text
+                              me="auto"
+                              color={
+                                activeRoute(route.path.toLowerCase())
+                                  ? theme.mainText
+                                  : theme.inactiveColor
+                              }
+                              fontWeight={
+                                activeRoute(route.path.toLowerCase())
+                                  ? "bold"
+                                  : "normal"
+                              }
+                            >
+                              {route.name}
+                            </Text>
+                          </Flex>
+                          <Box
+                            h="36px"
+                            w="4px"
+                            borderRadius="5px"
+                            bg={
+                              activeRoute(route.path.toLowerCase())
+                                ? theme.activeColor
+                                : "transparent"
+                            }
+                          />
+                        </HStack>
+                      </Box>
+                    </NavLink>
+                  ))}
+                </Box>
               </DrawerBody>
             </DrawerContent>
           </Drawer>
           <Flex alignItems="center">
-            <Text
-              // fontSize="1rem"
-              fontWeight="bold"
-              marginLeft="10px"
-            >
+            <Text fontWeight="bold" marginLeft="10px">
               {getActivePageName(location.pathname)}
             </Text>
           </Flex>
