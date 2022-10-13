f9c7e791c864581e5fecbd122228c6001f91fb8b
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-14 08:26:57 +0900

    update: responsive

--- src/components/register/InputField.tsx
+++ src/components/register/InputField.tsx
@@ -1,11 +1,13 @@
 import {
   Box,
   Button,
+  Flex,
   Input,
   InputGroup,
   InputRightElement,
 } from "@chakra-ui/react";
 import { SpinnerComponent } from "components/SpinnerComponent";
+import { useMobileContext } from "contexts/MobileContext";
 import { ChangeEvent, Dispatch, SetStateAction } from "react";
 import { useStack } from "services/stack/useStack";
 import { theme } from "theme/theme";
@@ -17,46 +19,88 @@ type InputFieldProps = {
 };
 
 export const InputField = ({ inputValue, setInputValue }: InputFieldProps) => {
+  const { isMobile } = useMobileContext();
+
   const { postStack, deleteStack, registerLoading } = useStack();
+
   return (
     <Box w="70%" h="30%" minHeight="50px" display="flex" alignItems="center">
-      <InputGroup size="lg">
-        <Input
-          pr="4.5rem"
-          type="number"
-          placeholder="Enter ISBN"
-          h="3rem"
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setInputValue(Number(event.target.value))
-          }
-        />
-        <InputRightElement width="10rem" bottom="0" margin="auto">
-          {registerLoading ? (
-            <SpinnerComponent />
-          ) : (
-            <>
-              <Button
-                bg={theme.subColor}
-                h="1.75rem"
-                size="md"
-                marginRight="10px"
-                onClick={() => postStack(inputValue)}
-              >
-                {words.register.stackButtonName}
-              </Button>
-              <Button
-                bg={theme.subColor}
-                h="1.75rem"
-                size="md"
-                marginRight="10px"
-                onClick={() => deleteStack(inputValue)}
-              >
-                {words.register.unstackButtonName}
-              </Button>
-            </>
-          )}
-        </InputRightElement>
-      </InputGroup>
+      {!isMobile ? (
+        <>
+          <InputGroup size="lg">
+            <Input
+              pr="4.5rem"
+              type="number"
+              placeholder="Enter ISBN"
+              h="3rem"
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setInputValue(Number(event.target.value))
+              }
+            />
+            <InputRightElement width="10rem" bottom="0" margin="auto">
+              {registerLoading ? (
+                <SpinnerComponent />
+              ) : (
+                <>
+                  <Button
+                    bg={theme.subColor}
+                    h="1.75rem"
+                    size="md"
+                    marginRight="10px"
+                    onClick={() => postStack(inputValue)}
+                  >
+                    {words.register.stackButtonName}
+                  </Button>
+                  <Button
+                    bg={theme.subColor}
+                    h="1.75rem"
+                    size="md"
+                    marginRight="10px"
+                    onClick={() => deleteStack(inputValue)}
+                  >
+                    {words.register.unstackButtonName}
+                  </Button>
+                </>
+              )}
+            </InputRightElement>
+          </InputGroup>
+        </>
+      ) : (
+        <Box>
+          <Input
+            pr="4.5rem"
+            type="number"
+            placeholder="Enter ISBN"
+            h="3rem"
+            marginBottom="20px"
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setInputValue(Number(event.target.value))
+            }
+          />
+          <Flex justifyContent="space-around">
+            {registerLoading ? (
+              <SpinnerComponent />
+            ) : (
+              <>
+                <Button
+                  bg={theme.subColor}
+                  size="md"
+                  onClick={() => postStack(inputValue)}
+                >
+                  {words.register.stackButtonName}
+                </Button>
+                <Button
+                  bg={theme.subColor}
+                  size="md"
+                  onClick={() => deleteStack(inputValue)}
+                >
+                  {words.register.unstackButtonName}
+                </Button>
+              </>
+            )}
+          </Flex>
+        </Box>
+      )}
     </Box>
   );
 };
