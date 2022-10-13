638cff288230f630a64f30c3ad0fc05f7c7f2615
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-14 08:16:20 +0900

    update: responsive

--- src/components/dashboard/books/MonthlyBookBarChart.tsx
+++ src/components/dashboard/books/MonthlyBookBarChart.tsx
@@ -68,7 +68,7 @@ export const MonthlyBookBarChart = ({ data, isLoading }: Props) => {
       isLoading={isLoading}
     >
       <BarChart
-        width={isMobile ? 300 : 200}
+        width={isMobile ? 300 : 400}
         height={isMobile ? 250 : 300}
         data={barChartData}
         barSize={isMobile ? 15 : 20}
