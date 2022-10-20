acef73ba316509111a509ee495454aa2e38b9ea1
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-21 08:19:40 +0900

    update: add searchResultField

--- src/components/register/StackRegister.tsx
+++ src/components/register/StackRegister.tsx
@@ -1,15 +1,28 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { useState } from "react";
 import { Card } from "../Card";
 import { InputField } from "./InputField";
+import { SearchResultField } from "./SearchResultField";
 
 export const StackRegister = () => {
   const [inputValue, setInputValue] = useState<number | null>(null);
 
   return (
     <Box>
-      <Card heightPixel={400}>
-        <InputField setInputValue={setInputValue} inputValue={inputValue!} />
+      <Card>
+        <Flex
+          w="100%"
+          h="100%"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          marginTop="20%"
+        >
+          <InputField setInputValue={setInputValue} inputValue={inputValue!} />
+          <Flex marginTop="30px" marginBottom="20px">
+            <SearchResultField inputValue={inputValue} />
+          </Flex>
+        </Flex>
       </Card>
     </Box>
   );
