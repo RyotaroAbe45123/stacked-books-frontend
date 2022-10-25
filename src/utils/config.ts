5e5bbb6fb0135279136889b828b95548bd973113
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-25 21:41:59 +0900

    fix: set mobile max width

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,3 +7,5 @@ export const openbdEndpoint = "https://api.openbd.jp/v1/get";
 export const openbdBookImageEndpoint = "https://cover.openbd.jp/";
 
 export const issBookImageEndpoint: string = "https://iss.ndl.go.jp/thumbnail/";
+
+export const mobileMaxWidth = 450;
