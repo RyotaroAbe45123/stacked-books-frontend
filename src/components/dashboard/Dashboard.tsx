49bf898faf5f36e0fe0cd74bc8e4ca176a51440d
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-30 08:37:12 +0900

    update: add CodeGraph to dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -6,11 +6,12 @@ import { MonthlyNumberOfBooks } from "./books/MonthlyNumberOfBooks";
 import { TotalNumberOfBooks } from "./books/TotalNumberOfBooks";
 import { TotalPagesOfBooks } from "./pages/TotalPagesOfBooks";
 import { TotalPriceOfBooks } from "./price/TotalPriceOfBooks";
-import { MonthlyBookBarChart } from "./books/MonthlyBookBarChart";
+// import { MonthlyBookBarChart } from "./books/MonthlyBookBarChart";
 import { MonthlyPriceOfBooks } from "./price/MonthlyPriceOfBooks";
 import { MonthlyPagesOfBooks } from "./pages/MonthlyPagesOfBooks";
 import { useMobileContext } from "contexts/MobileContext";
 import { MobileBookBarChart } from "./books/MobileBookBarChart";
+import { CodeGraph } from "./CodeGraph";
 
 export const Dashboard = () => {
   const { isMobile } = useMobileContext();
@@ -19,6 +20,30 @@ export const Dashboard = () => {
 
   return (
     <Box>
+      {/* <a
+        className="twitter-share-button"
+        href="https://twitter.com/intent/tweet"
+      >
+        Tweet
+      </a>
+      <a
+        className="twitter-share-button"
+        href="https://twitter.com/intent/tweet?text=Hello%20world"
+      >
+        Tweet2
+      </a>
+      <a
+        className="twitter-share-button"
+        href="https://twitter.com/intent/tweet?text=Hello%20world"
+        data-size="large"
+      >
+        Tweet3
+      </a>
+      <a
+        id="b"
+        className="btn"
+        href="https://twitter.com/intent/tweet?original_referer=https%3A%2F%2Fdeveloper.twitter.com%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&text=Guides%20%7C%20Docs%20%7C%20Twitter%20Developer%20Platform&url=https%3A%2F%2Fdeveloper.twitter.com%2Fen%2Fdocs%2Ftwitter-for-websites%2Ftweet-button%2Foverview"
+      ></a> */}
       <SimpleGrid columns={{ sm: 1, lg: 3 }} gap="20px" mb="20px">
         <TotalNumberOfBooks data={stacks} isLoading={isLoading} />
         <TotalPriceOfBooks data={stacks} isLoading={isLoading} />
@@ -33,7 +58,8 @@ export const Dashboard = () => {
         {!isMobile ? (
           <>
             <DailyBookBarChart data={stacks} isLoading={isLoading} />
-            <MonthlyBookBarChart data={stacks} isLoading={isLoading} />
+            <CodeGraph data={stacks} isLoading={isLoading} />
+            {/* <MonthlyBookBarChart data={stacks} isLoading={isLoading} /> */}
           </>
         ) : (
           <MobileBookBarChart data={stacks} isLoading={isLoading} />
