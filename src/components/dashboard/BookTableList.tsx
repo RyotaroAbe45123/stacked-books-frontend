ad9c81754a58782a719c864de1fef8fe6e8a6ca8
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-11-06 19:48:01 +0900

    update: add authors to table list

--- src/components/dashboard/BookTableList.tsx
+++ src/components/dashboard/BookTableList.tsx
@@ -9,26 +9,33 @@ import {
 } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 import { MdTableRows } from "react-icons/md";
-import { Stack } from "types/api";
+import { useBook } from "services/book/useBook";
+import { Book, Stack } from "types/api";
 import { TableListDataType } from "types/data";
 import { words } from "utils/words";
 import { Statistics } from "./Statistics";
 
 type Props = {
-  data: Stack[] | undefined;
+  stacks: Stack[] | undefined;
   isLoading: boolean;
 };
 
 const emptyData: TableListDataType = {
   date: "",
   title: "",
+  authors: "",
   price: 0,
   pages: 0,
 };
 
 const emptyArray: TableListDataType[] = Array(5).fill(emptyData);
 
-export const BookTableList = ({ data, isLoading }: Props) => {
+export const BookTableList = ({ stacks, isLoading }: Props) => {
+  const { books } = useBook({
+    offset: 0,
+    pageSize: 5,
+  });
+
   const [tableListData, setTableListData] =
     useState<TableListDataType[]>(emptyArray);
 
@@ -37,38 +44,50 @@ export const BookTableList = ({ data, isLoading }: Props) => {
     return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
   };
 
+  const searchAuthors = (isbn: number, books: Book[]) => {
+    const authors = books.filter((book) => book.isbn === isbn);
+    return authors ? authors[0]["authors"].join() : "";
+  };
+
   // 最新の5件を選択
-  const selectData = useCallback((stacks: Stack[]): TableListDataType[] => {
-    if (stacks.length > 4) {
-      const filteredStacks = stacks.slice(0, 5);
-      const tableListData: TableListDataType[] = filteredStacks.map((stack) => {
-        return {
-          date: convertTimeStampToDate(stack.timestamp),
-          title: stack.title,
-          price: stack.price,
-          pages: stack.pages,
-        };
-      });
-      return tableListData;
-    } else {
-      const initializedArray: TableListDataType[] = Array(5).fill(emptyData);
-      stacks.forEach((stack, i) => {
-        initializedArray[i] = {
-          date: convertTimeStampToDate(stack.timestamp),
-          title: stack.title,
-          price: stack.price,
-          pages: stack.pages,
-        };
-      });
-      return initializedArray;
-    }
-  }, []);
+  const selectData = useCallback(
+    (stacks: Stack[], books: Book[]): TableListDataType[] => {
+      if (stacks.length > 4) {
+        const filteredStacks = stacks.slice(0, 5);
+        const tableListData: TableListDataType[] = filteredStacks.map(
+          (stack) => {
+            return {
+              date: convertTimeStampToDate(stack.timestamp),
+              title: stack.title,
+              authors: searchAuthors(stack.isbn, books),
+              price: stack.price,
+              pages: stack.pages,
+            };
+          },
+        );
+        return tableListData;
+      } else {
+        const initializedArray: TableListDataType[] = Array(5).fill(emptyData);
+        stacks.forEach((stack, i) => {
+          initializedArray[i] = {
+            date: convertTimeStampToDate(stack.timestamp),
+            title: stack.title,
+            authors: searchAuthors(stack.isbn, books),
+            price: stack.price,
+            pages: stack.pages,
+          };
+        });
+        return initializedArray;
+      }
+    },
+    [],
+  );
 
   useEffect(() => {
-    if (data !== undefined) {
-      setTableListData(selectData(data));
+    if (stacks !== undefined && books !== undefined) {
+      setTableListData(selectData(stacks, books));
     }
-  }, [data, selectData]);
+  }, [stacks, books, selectData]);
 
   return (
     <Statistics
@@ -82,6 +101,7 @@ export const BookTableList = ({ data, isLoading }: Props) => {
             <Tr>
               <Th>{words.dashboard.books.tableColumns.date}</Th>
               <Th>{words.dashboard.books.tableColumns.title}</Th>
+              <Th>authors</Th>
               <Th>{words.dashboard.books.tableColumns.price}</Th>
               <Th>{words.dashboard.books.tableColumns.pages}</Th>
             </Tr>
@@ -92,14 +112,21 @@ export const BookTableList = ({ data, isLoading }: Props) => {
                 key={data.title ? data.title : `${data.price}/${index}`}
                 height="3rem"
               >
-                <Td maxWidth="150px">{data.date}</Td>
+                <Td maxWidth="100px">{data.date}</Td>
                 <Td
-                  width="500px"
-                  maxWidth="500px"
+                  width="450px"
+                  maxWidth={{ md: "200px", lg: "300px", xl: "400px" }}
                   overflow="hidden"
                   textOverflow="ellipsis"
                 >
                   {data.title}
+                </Td>
+                <Td
+                  maxWidth={{ md: "100px", lg: "150px", xl: "200px" }}
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                >
+                  {data.authors}
                 </Td>
                 <Td>{data.price ? data.price.toLocaleString() : ""}</Td>
                 <Td>{data.pages ? data.pages.toLocaleString() : ""}</Td>
